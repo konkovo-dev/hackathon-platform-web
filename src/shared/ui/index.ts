483defcec0b01620cd54f1e8646a26fad31c89ae
Nikita Kitaev <nik_kitaev@mail.ru>
2026-01-21 22:04:29 +0300

    feat(fields): add form field components and use search icon asset

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,4 +1,4 @@
-export { Button, type ButtonProps } from './Button'
+export { Button, type ButtonProps, type ButtonVariant, type ButtonSize } from './Button'
 export {
   Card,
   CardHeader,
@@ -7,8 +7,12 @@ export {
   type CardHeaderProps,
   type CardContentProps,
 } from './Card'
+export { Checkbox, type CheckboxProps } from './Checkbox'
 export { FormField, type FormFieldProps } from './FormField'
-export { Input, type InputProps } from './Input'
+export { Input, type InputProps, type InputVariant } from './Input'
+export { InputLabel, type InputLabelProps } from './InputLabel'
 export { Label, type LabelProps } from './Label'
+export { Radio, type RadioProps } from './Radio'
+export { LocaleToggle } from './LocaleToggle'
 export { ThemeToggle } from './ThemeToggle'
 export { Typography, type TypographyProps, type TypographyVariant } from './Typography'
